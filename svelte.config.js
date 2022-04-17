543f3bb325ca84ae21917825cc6bd8a2259b0038
Jesse-Richman <shadowarcher4@gmail.com>
2022-04-17 22:50:24 +0000

    Fix vite configuration.

--- svelte.config.js
+++ svelte.config.js
@@ -17,8 +17,9 @@ const config = {
 		vite: {
 			server: {
 				hmr: {
-					clientPort: process.env.HMR_HOST ? 443 : 3000,
-					host: process.env.HMR_HOST ? process.env.HMR_HOST.substring("https://".length) : "localhost"
+					host: process.env.HMR_HOST ? process.env.HMR_HOST.substring("https://".length) : "localhost",
+					protocol: 'wss',
+					clientPort: process.env.HMR_HOST ? 443 : 3000
 				}
 			}
 		}
